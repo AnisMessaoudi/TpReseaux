e71924ef0a013ce99d6daa78c9624e704e550c2d
thenjip <39303840+thenjip@users.noreply.github.com>
2018-11-21 11:34:18 +0100

    Implements server's BankWriter writeX methods.

--- fr.ensisa.hassenforder.transportation.server.empty/src/fr/ensisa/hassenforder/transportation/server/network/BankWriter.java
+++ fr.ensisa.hassenforder.transportation.server.empty/src/fr/ensisa/hassenforder/transportation/server/network/BankWriter.java
@@ -5,10 +5,20 @@ import java.io.OutputStream;
 import fr.ensisa.hassenforder.network.BasicAbstractWriter;
 import fr.ensisa.hassenforder.transportation.bank.network.Protocol;
 
-public class BankWriter extends BasicAbstractWriter {
+public class BankWriter extends BasicAbstractWriter
+{
+  public BankWriter(OutputStream outputStream)
+  {
+    super(outputStream);
+  }
 
-    public BankWriter(OutputStream outputStream) {
-        super(outputStream);
-    }
 
+
+  public void writeBankWithdrawRequest(long cardId, int amount)
+  {
+    this.writeInt(Protocol.REQ_BANK_WITHDRAW);
+
+    this.writeLong(cardId);
+    this.writeInt(amount);
+  }
 }
