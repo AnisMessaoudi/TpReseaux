d732ba796d9f8aa831180827b73d0327ba30a274
Anis <muhammed-anis.messaoudi@uha.fr>
2018-11-24 16:06:34 +0100

    add read/write methods in Kiosk

--- fr.ensisa.hassenforder.transportation.kiosk.empty/src/fr/ensisa/hassenforder/transportation/kiosk/network/KioskReader.java
+++ fr.ensisa.hassenforder.transportation.kiosk.empty/src/fr/ensisa/hassenforder/transportation/kiosk/network/KioskReader.java
@@ -10,14 +10,87 @@ import fr.ensisa.hassenforder.transportation.kiosk.model.Transaction;
 
 public class KioskReader extends BasicAbstractReader {
 
+    private Pass pass;
+
     public KioskReader(InputStream inputStream) {
         super(inputStream);
     }
 
     public void receive() {
         type = readInt();
-        switch (type) {
+        switch (type)
+        {
+        case Protocol.REP_OK:
+          break;
+        case Protocol.REP_KO:
+          break;
+        case Protocol.REP_PASS :
+          readPassReply();
+          break;
+        case Protocol.REP_NEW_PASS :
+          readNewPassReply();
+          break;
+        case Protocol.REP_TRANSACTION :
+          readTransactionReply();
+          break;
+        case Protocol.REP_PASS_ID :
+          readPassIdReply();
+          break;
         }
+    }
+
+    private void readPassReply()
+    {
+      long passId = readLong();
+      String description = readString();
+      pass = new Pass(passId, description);
+      int size = readInt();
+      if (size != 0 )
+      {
+        for (int i = 0; i < size; i++) 
+        {
+          Ticket.Type type = Ticket.Type.values() [readInt()];
+          String id = readString();
+          String from = readString();
+          String to = readString();
+          Ticket.Month month = Ticket.Month.values()[readInt()];
+          int count = readInt();
+          int used = readInt();
+          switch (type)
+          {
+          case ROUTE :
+            pass.addTicket(new Ticket(id,from,to,count,used));
+            break;
+          case URBAN : 
+            pass.addTicket(new Ticket(id,count,used));
+            break;
+          case SUBSCRIPTION : 
+            pass.addTicket(new Ticket(id,month,used));
+            break;
+          default:
+            break;
+          } // END switch
+        } // END For
+      } // END if
+    }
+
+    private void readNewPassReply()
+    {
+      long passId = readLong();
+      
+    }
+
+    private void readTransactionReply()
+    {
+      long id= readLong();
+      int amount = readInt();
+      
+    }
+
+    private void readPassIdReply()
+    {
+      long passId= readLong();
+      
     }
 
 }
--- fr.ensisa.hassenforder.transportation.kiosk.empty/src/fr/ensisa/hassenforder/transportation/kiosk/network/KioskWriter.java
+++ fr.ensisa.hassenforder.transportation.kiosk.empty/src/fr/ensisa/hassenforder/transportation/kiosk/network/KioskWriter.java
@@ -13,12 +13,48 @@ public class KioskWriter extends BasicAbstractWriter {
 
     public void createNewPass()
     {
-      
+      writeInt(Protocol.REQ_NEW_PASS);
     }
     
     public void createGetPassbyId(long passId)
     {
-      writeInt(Protocol.REQ_GET_PASS_BY_ID_K);
+      writeInt(Protocol.REQ_GET_PASS_BY_ID);
       writeLong(passId);
+    }
+    
+    public void createBuyRoute(long passId, String from, String to, int count)
+    {
+      writeInt(Protocol.REQ_BUY_ROUTE);
+      writeLong(passId);
+      writeString(from);
+      writeString(to);
+      writeInt(count);
+    }
+    
+    public void createBuyUrban(long passId, int count)
+    {
+      writeInt(Protocol.REQ_BUY_URBAN);
+      writeLong(passId);
+      writeInt(count);
+    }
+    
+    public void createBuySubscription(long passId, int month)
+    {
+      writeInt(Protocol.REQ_BUY_SUBSCRIPTION);
+      writeLong(passId);
+      writeInt(month);
+    }
+    
+    public void createPayTransaction(long id, long cardId)
+    {
+      writeInt(Protocol.REQ_PAY_TRANSACTION);
+      writeLong(id);
+      writeLong(cardId);
+    }
+    
+    public void createCancelTransaction(long id)
+    {
+      writeInt(Protocol.REQ_CANCEL_TRANSACTION);
+      writeLong(id);
     }
 }
