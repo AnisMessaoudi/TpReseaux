aa509f9e37c951f4d7a5da3e26b8f0cfa8914e5f
thenjip <39303840+thenjip@users.noreply.github.com>
2018-11-24 15:06:11 +0100

    Implement bank's BankSession operate method.

--- fr.ensisa.hassenforder.transportation.bank.empty/src/fr/ensisa/hassenforder/transportation/bank/network/BankSession.java
+++ fr.ensisa.hassenforder.transportation.bank.empty/src/fr/ensisa/hassenforder/transportation/bank/network/BankSession.java
@@ -1,59 +1,99 @@
 package fr.ensisa.hassenforder.transportation.bank.network;
 
+
+
 import java.io.IOException;
 import java.net.Socket;
 
 import fr.ensisa.hassenforder.transportation.bank.BankNetworkListener;
 import fr.ensisa.hassenforder.transportation.bank.network.Protocol;
+import fr.ensisa.hassenforder.transportation.bank.network.BankReader.BankWithdrawRequest;
 
-public class BankSession extends Thread {
 
-	private Socket connection;
-	private BankNetworkListener listener;
-	
-	public BankSession(Socket connection, BankNetworkListener listener) {
-		this.connection = connection;
-		this.listener = listener;
-		if( listener == null) throw new RuntimeException("listener cannot be null");
-	}
 
-	public void close () {
-		this.interrupt();
-		try {
-			if (connection != null)
-				connection.close();
-		} catch (IOException e) {
-		}
-		connection = null;
-	}
+public class BankSession extends Thread
+{
+  private Socket connection;
+  private BankNetworkListener listener;
 
-	private void processRequestWithdraw(BankReader reader, BankWriter writer) {
-	}
 
-	public boolean operate() {
-		try {
-			BankWriter writer = new BankWriter (connection.getOutputStream());
-			BankReader reader = new BankReader (connection.getInputStream());
-			reader.receive ();
-			switch (reader.getType ()) {
-			case 0 : return false; // socket closed
-			}
-			writer.send ();
-			return true;
-		} catch (IOException e) {
-			return false;
-		}
-	}
 
-	public void run() {
-		while (true) {
-			if (! operate())
-				break;
-		}
-		try {
-			if (connection != null) connection.close();
-		} catch (IOException e) {
-		}
-	}
+  public BankSession(Socket connection, BankNetworkListener listener) {
+    this.connection = connection;
+    this.listener = listener;
+    if( listener == null) throw new RuntimeException("listener cannot be null");
+  }
 
+
+
+  public void close () {
+    this.interrupt();
+    try {
+      if (connection != null)
+        connection.close();
+    } catch (IOException e) {
+    }
+    connection = null;
+  }
+
+
+  private void processRequestWithdraw(BankReader reader, BankWriter writer)
+  {
+    final BankWithdrawRequest request =
+      (BankWithdrawRequest) reader.extractRequest()
+    ;
+
+    if (request != null) {
+      final int amount = request.amount;
+
+      if (amount >= 0) {
+        if (this.listener.withdrawByCardId(request.cardId, amount)) {
+          writer.writeOkReply();
+          return;
+        }
+      }
+    }
+
+    writer.writeKoReply();
+  }
+
+
+  public boolean operate()
+  {
+    try {
+      BankWriter writer = new BankWriter(connection.getOutputStream());
+      BankReader reader = new BankReader(connection.getInputStream());
+
+      reader.receive ();
+      switch (reader.getType()) {
+        case 0:
+          return false; // socket closed
+
+        default:
+          break;
+
+        case Protocol.REQ_BANK_WITHDRAW:
+          this.processRequestWithdraw(reader, writer);
+          break;
+      }
+
+      writer.send();
+      return true;
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+
+  public void run()
+  {
+    while (true) {
+      if (! operate())
+        break;
+    }
+    try {
+      if (connection != null) connection.close();
+    } catch (IOException e) {
+    }
+  }
 }
